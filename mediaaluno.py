4f87f314568a2938cb375ee86fafea5ae8c757bf
luzia <luzia.s.rocha@aluno.senai.br>
2024-11-28 10:50:16 -0500

    fazendo o ssegundo dia de programação da media do aluno

--- mediaaluno.py
+++ mediaaluno.py
@@ -1,8 +1,19 @@
 cont = int(input())
-nome = string (input())
+nome = input()
 nota1 = float (input())
 nota2 = float(input())
 nota3 = float(input())
 nota4 = float (input())
 falta = int(input())
+soma = (nota1+nota2+nota3+nota4)/4
+if soma >=8 and falta <30:
+    print(" você foi aprovado") 
+elif soma >5 and falta <30:
+ print(" você fivou de recuperação")
+recuperacao = float (input())
+calculo = (10 - soma)
+if recuperacao > calculo:
+    print("reprovado na recuperação")
+else:
+    print("reprovado na recuperação")
 
