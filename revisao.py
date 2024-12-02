e6379bb3c834522816efa8a3e7651cfd38e49746
unknown <luzia.s.rocha@aluno.senai.br>
2024-12-02 10:53:19 -0500

    ler os comandos

--- revisao.py
+++ revisao.py
@@ -9,4 +9,22 @@ nota4 = float(input())#ler as notas
 faltas = int(input())
 #calculo da media
 media = (nota1+nota2+nota3+nota4)/4
-print(media)
+#lógica da situação 
+if faltas >= 31:
+    situacao ="reprovado por falta"
+elif media >=8:
+     situacao = "aprovado"
+elif media >=5: #recuperação
+    recupecao = float(input()) #ler a nota da prova de recuperção
+    if recupecao >= (10-media):
+        situacao ="aprovado na reuperação"
+    else:
+        situacao = "reprovado na recuperação"
+else:
+    situacao = "reprovado por média"
+# relatório
+print("nome:", nome)
+print("notas:", nota1,nota2,nota3,nota4)
+print("faltas:", faltas)
+print("média:",media)
+print("situação",situacao)
