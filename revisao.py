02b51be58642847f3d941ea23bd4b522a9968877
unknown <luzia.s.rocha@aluno.senai.br>
2024-12-02 10:09:30 -0500

    ler os comandos

---
+++ revisao.py
@@ -0,0 +1,12 @@
+# ler entrada do usuário
+nome = input() #armazenar o nome do aluno
+print(nome)
+nota1 = float(input())#ler as notas
+nota2 = float(input())#ler as notas
+nota3 = float(input())#ler as notas
+nota4 = float(input())#ler as notas
+
+faltas = int(input())
+#calculo da media
+media = (nota1+nota2+nota3+nota4)/4
+print(media)
