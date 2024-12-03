5d299c730a7bf13f283bdd40a1a00aae1fb1c583
luzia <luzia.s.rocha@aluno.snai.br>
2024-12-03 09:41:29 -0500

    programação concluida

--- revisao.py
+++ revisao.py
@@ -1,30 +1,34 @@
 # ler entrada do usuário
-nome = input() #armazenar o nome do aluno
-print(nome)
-nota1 = float(input())#ler as notas
-nota2 = float(input())#ler as notas
-nota3 = float(input())#ler as notas
-nota4 = float(input())#ler as notas
+cont = 0 #variavel que controla  a repetição
+escolha_usuario = int(input()) # variavel que  guarda quantas vezes  o codigo vai rodar 
+while cont <escolha_usuario:
+    nome = input() #armazenar o nome do aluno
+    print(nome)
+    nota1 = float(input())#ler as notas
+    nota2 = float(input())#ler as notas
+    nota3 = float(input())#ler as notas
+    nota4 = float(input())#ler as notas
 
-faltas = int(input())
-#calculo da media
-media = (nota1+nota2+nota3+nota4)/4
-#lógica da situação 
-if faltas >= 31:
-    situacao ="reprovado por falta"
-elif media >=8:
-     situacao = "aprovado"
-elif media >=5: #recuperação
-    recupecao = float(input()) #ler a nota da prova de recuperção
-    if recupecao >= (10-media):
-        situacao ="aprovado na reuperação"
+    faltas = int(input())
+    #calculo da media
+    media = (nota1+nota2+nota3+nota4)/4
+    #lógica da situação 
+    if faltas >= 31:
+        situacao ="reprovado por falta"
+    elif media >=8:
+        situacao = "aprovado"
+    elif media >=5: #recuperação
+        recupecao = float(input()) #ler a nota da prova de recuperção
+        if recupecao >= (10-media):
+            situacao ="aprovado na reuperação"
+        else:
+            situacao = "reprovado na recuperação"
     else:
-        situacao = "reprovado na recuperação"
-else:
-    situacao = "reprovado por média"
-# relatório
-print("nome:", nome)
-print("notas:", nota1,nota2,nota3,nota4)
-print("faltas:", faltas)
-print("média:",media)
-print("situação",situacao)
+        situacao = "reprovado por média"
+    # relatório
+    print("nome:", nome)
+    print("notas:", nota1,nota2,nota3,nota4)
+    print("faltas:", faltas)
+    print("média:",media)
+    print("situação",situacao)
+    cont = cont+1
