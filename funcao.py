f1d54f44ccf67df0a85bf389146697fedbb8679e
luzia <luzia.s.rocha@aluno.senai.br>
2024-12-11 08:46:54 -0500

    primeira função

---
+++ funcao.py
@@ -0,0 +1,13 @@
+#declarar uma função
+def bom_dia (): 
+    print("bom dia!")
+
+def boa_noite ():
+    print("boa noite!")
+    
+def boa_tarde():
+    print("boa tarde!")
+
+bom_dia()
+boa_noite()
+boa_tarde()
